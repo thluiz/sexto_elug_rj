088cc2adb0e295c69e1e814f1562a16e97387386
Thiago Luiz Silva <contato@thluiz.com>
2016-06-26 19:40:56 -0300

    fixing repo configuration for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :sexto_elug_rj, SextoElugRj.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :hello_phoenix, SextoElugRj.Repo,
+config :sexto_elug_rj, SextoElugRj.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
