db3b4c00c72b9cb1fd1c18b172a333277660679e
Thiago Luiz Silva <contato@thluiz.com>
2016-06-26 19:58:30 -0300

    trying to fix 403 on socket handshake

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :sexto_elug_rj, SextoElugRj.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "floating-shore-58253.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
